1904c00fd461c4393533efdeac356971b914f12d
Nikita TURKMEN <info@nikitaturkmen.com>
2022-08-22 19:32:18 +0300

    son degisiklikler

--- 1-QT/main.py
+++ 1-QT/main.py
@@ -1,12 +1,8 @@
-import sys
+from PySide2.QtWidgets import QApplication, QMainWindow
+from statik import HesapMakinesi
 
-from PySide2.QtWidgets import QApplication, QWidget
 
-if __name__ == "__main__":
-    # Create the Qt Application
-    app = QApplication(sys.argv)
-    # Create and show the form
-    form = QWidget()
-    form.show()
-    # Run the main Qt loop
-    sys.exit(app.exec_())
+
+app = QApplication()
+
+app.exec_()
